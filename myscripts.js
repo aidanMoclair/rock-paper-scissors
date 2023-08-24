226437c80e699c32eecc5ee2579ec95a1a0f6f95
aidanMoclair <138340761+aidanMoclair@users.noreply.github.com>
2023-08-24 11:04:52 -0400

    added the playRound function so that it takes a playerSelection and computerSelection and returns either win, loss, or draw.

--- myscripts.js
+++ myscripts.js
@@ -15,5 +15,61 @@ function getComputerChoice()
         return "scissors";
     }
 }
-
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection=playerSelection.toLowerCase()
+
+    if(playerSelection== "rock")
+    {
+        if(computerSelection=="rock")
+        {
+            return "draw"
+        }
+        else if(computerSelection=="paper")
+        {
+            return "loss"
+        }
+        else
+        {
+            return "win"
+        }
+    }
+    else if(playerSelection== "paper")
+    {
+        if(computerSelection=="rock")
+        {
+            return "win"
+        }
+        else if(computerSelection=="paper")
+        {
+            return "draw"
+        }
+        else
+        {
+            return "loss"
+        }
+    }
+    else
+    {
+        if(computerSelection=="rock")
+        {
+            return "loss"
+        }
+        else if(computerSelection=="paper")
+        {
+            return "win"
+        }
+        else
+        {
+            return "draw"
+        }
+    }   
+}
+
+    let player="rock";
+    let computer=getComputerChoice();
+
+    console.log(computer);
+    console.log(playRound(player, computer));
